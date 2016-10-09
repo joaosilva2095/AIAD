1ef431082734a1b730b74a227725fa8a740039ab
João Silva <joaosilva@revtut.net>
2016-10-09 18:53:25 +0100

    Added documentation. Updated objects.

--- src/aiad/feup/Company.java
+++ src/aiad/feup/Company.java
@@ -1,17 +1,51 @@
 package aiad.feup;
 
+import java.util.Random;
+
 /**
  * A company.
+ * Holds information about the share.
  */
 public class Company {
 
     /**
-     * Current stock prive
+     * Random utility class
+     */
+    private static final Random random = new Random();
+
+    /**
+     * Current stock price of the company
      */
     private int price;
+
+    /**
+     * Fluctuation of the stock price of the company
+     */
+    private double fluctuation;
 
     /**
      * True if no new negotiation is allowed
      */
     private boolean closed;
+
+    /**
+     * Constructor of Company
+     * @param price stock price of the company
+     * @param fluctuation fluctuation of the company
+     */
+    public Company(int price, double fluctuation) {
+        this.price = price;
+        this.fluctuation = fluctuation;
+        this.closed = false;
+    }
+
+    /**
+     * Apply a fluctuation to the stock price of the company
+     */
+    public void applyFluctuation() {
+        int multiplier = random.nextBoolean() ? 1 : -1;
+        double offset = fluctuation * price;
+
+        price += multiplier * offset;
+    }
 }
